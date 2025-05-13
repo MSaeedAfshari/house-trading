5ae235645670b3e6bc498dfb545d81676dc51aea
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-05-13 09:56:58 +0330

    import new components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Features from "@/components/templates/index/Features";
+import Homes from "@/components/templates/index/Homes";
+import StoryPictures from "@/components/templates/index/StoryPictures";
 import React from "react";
 export default function index() {
   return (
     <>
       <Features />
+      <StoryPictures />
+      <Homes />
     </>
   );
 }
