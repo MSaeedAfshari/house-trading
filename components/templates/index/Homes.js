1013be8b2b8a1c9064fd9c94caac3bebbddea031
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-05-13 09:56:06 +0330

    create homes section

---
+++ components/templates/index/Homes.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+export default function Homes() {
+  return (
+    <div className="homes">
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <div className="card__details">
+          <p className="card__text">مالدیو</p>
+
+          <p className="card__text">4 اتاق</p>
+
+          <p className="card__text">400 متر مربع</p>
+
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <div className="card__details">
+          <p className="card__text">مالدیو</p>
+
+          <p className="card__text">4 اتاق</p>
+
+          <p className="card__text">400 متر مربع</p>
+
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <div className="card__details">
+          <p className="card__text">مالدیو</p>
+
+          <p className="card__text">4 اتاق</p>
+
+          <p className="card__text">400 متر مربع</p>
+
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <div className="card__details">
+          <p className="card__text">مالدیو</p>
+
+          <p className="card__text">4 اتاق</p>
+
+          <p className="card__text">400 متر مربع</p>
+
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <div className="card__details">
+          <p className="card__text">مالدیو</p>
+
+          <p className="card__text">4 اتاق</p>
+
+          <p className="card__text">400 متر مربع</p>
+
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <div className="card__details">
+          <p className="card__text">مالدیو</p>
+
+          <p className="card__text">4 اتاق</p>
+
+          <p className="card__text">400 متر مربع</p>
+
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+    </div>
+  );
+}
